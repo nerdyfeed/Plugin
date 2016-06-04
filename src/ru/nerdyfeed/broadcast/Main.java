dbc04406e74c7f5ffbaddf4e8b9145101486529f
nerdyfeed <nerdylivefeed@gmail.com>
2016-06-04 23:18:52 +0300

    Fixed bugs :+1:

---
+++ src/ru/nerdyfeed/broadcast/Main.java
@@ -0,0 +1,14 @@
+package ru.nerdyfeed.broadcast;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public void onEnable() {
+		Bukkit.getServer().getPluginManager();
+	}
+	public void onDisable() {
+		
+	}
+}
